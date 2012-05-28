af03aaa0dc3cd0504150ae359130db38b159a634
redCashion <xperts@gmail.com>
2012-05-28 14:41:47 -0400

    all tests pass

--- test/yAxis.spec.js
+++ test/yAxis.spec.js
@@ -1,4 +1,4 @@
-define(['jquery', 'dropchart', 'argsFor', 'YAxis'], function($, dropchart, argsFor, YAxis) {
+define(['jquery', 'dropchart', 'argsFor', 'YAxis', 'stub'], function($, dropchart, argsFor, YAxis, stub) {
   
   var should = chai.should();
   var yAxisSpec = {
@@ -7,27 +7,23 @@ define(['jquery', 'dropchart', 'argsFor', 'YAxis'], function($, dropchart, argsF
 
       describe('YAxis', function() {
 
-        before(function(done) {
-          this.args = {
-            canvas: argsFor.canvas(),
-            data: argsFor.data()
-          };
-          this.canvas = new dropchart.Canvas(this.args);
-          done();
+        before(function() {
+          this.canvas = stub.canvas();
         });
 
-        beforeEach(function(done) {
+        beforeEach(function() {
+          this.yAxis = new dropchart.YAxis(argsFor.histogram());
+          this.yAxis.render();
+        });
+
+        afterEach(function() {
           if (this.yAxis) {
             this.yAxis.destroy();
           }
-          this.args.yAxis = {};
-          done();
         });
 
         describe ("#destroy", function() {
           it ('should remove the yAxis from the canvas', function() {
-            this.args.yAxis = argsFor.yAxis();
-            this.yAxis = new dropchart.YAxis(this.args);
             $("#histogram").should.have('line.y-axis-line');
             this.yAxis.destroy();
             $("#histogram").should.not.have('line.y-axis-line');
@@ -38,12 +34,6 @@ define(['jquery', 'dropchart', 'argsFor', 'YAxis'], function($, dropchart, argsF
 
           describe('args.yAxis.show = true', function() {
 
-            beforeEach(function(done) {
-              this.args.yAxis = argsFor.yAxis();
-              this.yAxis = new dropchart.YAxis(this.args);
-              done();
-            });
-
             it ('should draw a line for the Y axis', function() {
               $("#histogram").should.have('line.y-axis-line');
             });
@@ -51,12 +41,11 @@ define(['jquery', 'dropchart', 'argsFor', 'YAxis'], function($, dropchart, argsF
             it ('should draw a Y axis line with the correct attributes', function() {
               var line = $('#histogram').find('line.y-axis-line');
               if (line) {
-                line.attr('stroke').should.equal("#000");
+                line.attr('stroke').should.equal("red");
                 line.attr('stroke-width').should.equal("2");
-                line.attr('x1').should.equal("5%");
-                line.attr('x2').should.equal("5%");
                 line.attr('y1').should.equal("0");
-                line.attr('y2').should.equal("100%");
+                line.attr('y2').should.equal("180");
+                line.attr('opacity').should.equal("1");
               } else {
                 $("#histogram").should.have('line.y-axis-line');
               }
@@ -67,10 +56,16 @@ define(['jquery', 'dropchart', 'argsFor', 'YAxis'], function($, dropchart, argsF
           describe('args.yAxis.show = false', function() {
 
             it ('should not draw a line for the Y axis', function() {
-              this.args.yAxis = {
-                show: false
-              };
-              this.yAxis = new dropchart.YAxis(this.args);
+              this.yAxis.destroy();
+              this.yAxis = new dropchart.YAxis({
+                chart: argsFor.chartHistogram(),
+                data: argsFor.data(),
+                canvas: argsFor.canvasWithInner(),
+                yAxis: {
+                  drawLine: false
+                }
+              });
+              this.yAxis.render();
               $('#histogram').should.not.have('line.y-axis-line');
             });
 
@@ -78,20 +73,14 @@ define(['jquery', 'dropchart', 'argsFor', 'YAxis'], function($, dropchart, argsF
 
           describe('args.yAxis.useTicks = true', function() {
 
-            beforeEach(function(done) {
-              this.args.yAxis = argsFor.yAxisWithTicks();
-              this.yAxis = new dropchart.YAxis(this.args);
-              done();
-            });
-
             it ('should draw the correct number of ticks on the Y axis', function() {
               $('line.y-axis-tick').length.should.equal(4);
             });
 
             it ('should place the ticks at the correct points on the axis', function() {
-              $('line.y-axis-tick:eq(1)').attr("y1").should.equal(0);
-              $('line.y-axis-tick:eq(2)').attr("y1").should.equal(0);
-              $('line.y-axis-tick:eq(3)').attr("y1").should.equal(0);
+              $('line.y-axis-tick:eq(1)').attr("y1").should.equal('45');
+              $('line.y-axis-tick:eq(2)').attr("y1").should.equal('90');
+              $('line.y-axis-tick:eq(3)').attr("y1").should.equal('135');
             });
 
           });
