794c0d7649d7c2ef227d0021d5d3887d5fbb554b
redCashion <xperts@gmail.com>
2012-05-29 12:30:50 -0400

    update tests

--- test/builder.spec.js
+++ test/builder.spec.js
@@ -11,12 +11,13 @@ define(
         describe('#initialize', function() {
 
           it ('should create the chart', function() {
-            console.log("jhi");
             new dropchart.Builder({
               data: argsFor.data()
             });
             $("#histogram").should.have('svg');
-            $("#histogram").should.have('g');
+            $("#histogram").should.have('g#histogram-group');
+            $("#histogram").should.have('g#x-axis-group');
+            $("#histogram").should.have('g#y-axis-group');
 
           });
 
